b8b44f2e70cc4a3ff909670ce9e1bc7eaf5b9de6
Elsa Barba <elsaxbarba@gmail.com>
2022-06-17 15:10:50 -0500

    proyecto saludo

--- html/saludo/script.js
+++ html/saludo/script.js
@@ -1,1 +1,16 @@
-console.log("Esta es una frase")
+console.log("Hola hola hola hola hola");
+
+setInterval( cambiarSaludo, 2000);
+
+function cambiarSaludo(){
+let frase = document.getElementsByTagName("h3")[0].innerHTML;
+
+    if (document.getElementsByTagName("h3")[0].innerHTML == "Awesome!"){
+        document.getElementsByTagName("h3")[0].innerHTML = "Nueva frase";
+    }
+
+    else {
+        document.getElementsByTagName("h3")[0].innerHTML = "Awesome!";
+    }
+    console.log("frase :" + frase)
+}
